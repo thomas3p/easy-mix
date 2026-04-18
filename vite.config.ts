d7a895a568b8df68e8627b090cbf899ff96d3635
Livem <Livem@LIVEMASTERU7>
2026-04-18 13:42:09 +0700

    Add gh-pages dependency and update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/easy-mix/",
   server: {
     host: "::",
     port: 8080,
